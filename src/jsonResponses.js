85757ca53b76bde5dd7e83ad5278113efc5360b4
Kevin Kulp <kmk8334@rit.edu>
2021-02-28 23:49:30 -0500

    Create API functionality

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,100 +1,172 @@
-// const fs = require('fs'); // Pull in the file system module
+// thanos snaps every time the server crashes (this is only in memory)
+const users = {};
 
+// GET request
 const respondJSON = (request, response, status, object) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
-  response.write(JSON.stringify(object));
-  response.end();
-};
-
-const respondXML = (request, response, status, object) => {
-  response.writeHead(status, { 'Content-Type': 'text/xml' });
-  console.dir(object);
-  response.write(object); // Object assumed to be a premade XML string
-  response.end();
-};
-
-const getSuccess = (request, response, params, acceptedTypes) => {
-  // Set the response data
-  const responseContent = {
-    message: 'This is a successful response',
-  };
-  const status = 200;
-
-  // Return XML, if requested
-  if (acceptedTypes[0] === 'text/xml') {
-    let responseXML = '<response>';
-    responseXML = `${responseXML} <message>${responseContent.message}</message>`;
-    responseXML = `${responseXML} </response>`;
-
-    console.log(responseXML);
-
-    return respondXML(request, response, status, responseXML);
-  }
-
-  // Return JSON
-  return respondJSON(request, response, status, responseContent);
-};
-
-const getBadRequest = (request, response, params, acceptedTypes) => {
-  // Set the response data
-  let responseContent = {
-    message: 'This request has the required parameters',
-  };
-  let status = 200;
-
-  // Apply query params
-  if (!params.valid || params.valid !== 'true') {
-    responseContent = {
-      id: 'badRequest',
-      message: 'Missing valid query parameter set to true',
+    const headers = {
+        'Content-Type': 'application/json',
     };
-    status = 400;
-  }
 
-  // Return XML, if requested
-  if (acceptedTypes[0] === 'text/xml') {
-    let responseXML = '<response>';
-    // If the error ID exists, display it
-    if (responseContent.id) {
-      responseXML = `${responseXML} <id>${responseContent.id}</id>`;
-    }
-    responseXML = `${responseXML} <message>${responseContent.message}</message>`;
-    responseXML = `${responseXML} </response>`;
-
-    return respondXML(request, response, status, responseXML);
-  }
-
-  // Return JSON
-  return respondJSON(request, response, status, responseContent);
+    response.writeHead(status, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(object));
+    response.end();
 };
 
-const getNotFound = (request, response, params, acceptedTypes) => {
-  // Set the response data
-  const responseContent = {
-    id: 'notFound',
-    message: 'The page you are looking for was not found.',
-  };
-  const status = 501;
+// HEAD request - Respond only with metadata about the request
+const respondJSONMeta = (request, response, status) => {
+    const headers = {
+        'Content-Type': 'application/json',
+    };
 
-  // Return XML, if requested
-  if (acceptedTypes[0] === 'text/xml') {
-    let responseXML = '<response>';
-    // If the error ID exists, display it
-    if (responseContent.id) {
-      responseXML = `${responseXML} <id>${responseContent.id}</id>`;
-    }
-    responseXML = `${responseXML} <message>${responseContent.message}</message>`;
-    responseXML = `${responseXML} </response>`;
-
-    return respondXML(request, response, status, responseXML);
-  }
-
-  // Return JSON
-  return respondJSON(request, response, status, responseContent);
+    response.writeHead(status, { 'Content-Type': 'application/json' });
+    response.end();
 };
+
+// GET /getUsers
+const getUsers = (request, response) => {
+    const responseJSON = {
+        users,
+    };
+
+    return respondJSON(request, response, 200, responseJSON);
+};
+
+// HEAD /getUsers
+const getUsersMeta = (request, response) => respondJSON(request, response, 200);
+
+const addUser = (request, response, body) => {
+    console.dir(body);
+
+    const responseJSON = {
+        message: 'Name and age are both required',
+    };
+
+    // Validate the name and age exist
+    if(!body.name || !body.age) {
+        responseJSON.id = 'missingParams';
+        return respondJSON(request, response, 400, responseJSON);
+    }
+
+    let responseCode = 201;
+
+    if(users[body.name]) {
+        // User already exists
+        responseCode = 204;
+    } else {
+        // User does not exist
+        users[body.name] = {};
+        users[body.name].name = body.name;
+    }
+
+    users[body.name].age = body.age;
+
+    // A new user was created
+    if(responseCode === 201) {
+        responseJSON.message = 'Created Successfully';
+        return respondJSON(request, response, responseCode, responseJSON);
+    }
+
+    // A user was updated
+    responseJSON.message = 'Updated Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+};
+
+const notFound = (request, response) => {
+    const responseJSON = {
+        message: 'The page you are not looking for was not found!',
+        id: 'notFound',
+    };
+
+    return respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
 
 module.exports = {
-  getSuccess,
-  getBadRequest,
-  getNotFound,
+    getUsers,
+    getUsersMeta,
+    addUser,
+    notFound,
+    notFoundMeta,
 };
+
+// const respondXML = (request, response, status, object) => {
+//   response.writeHead(status, { 'Content-Type': 'text/xml' });
+//   console.dir(object);
+//   response.write(object); // Object assumed to be a premade XML string
+//   response.end();
+// };
+
+// const getSuccess = (request, response, params, acceptedTypes) => {
+//   console.dir(request);
+//   // Set the response data
+//   const responseContent = {
+//     message: 'This is a successful response',
+//   };
+//   const status = 200;
+
+//   // Return JSON
+//   return respondJSON(request, response, status, responseContent);
+// };
+
+// const getUsers = (request, response, params, acceptedTypes) => {
+//     // Set the response data
+//     console.dir(request);
+//     const responseContent = {
+//       message: 'This is a successful response',
+//     };
+//     const status = 200;
+
+//     // Return JSON
+//     return respondJSON(request, response, status, responseContent);
+//   };
+
+// const createUser = (request, response, params, acceptedTypes) => {
+//     // Set the response data
+//     const responseContent = {
+//       message: 'Created Successfully',
+//     };
+//     const status = 201;
+
+//     // Return JSON
+//     return respondJSON(request, response, status, responseContent);
+//   };
+
+// const getBadRequest = (request, response, params, acceptedTypes) => {
+//   // Set the response data
+//   let responseContent = {
+//     message: 'This request has the required parameters',
+//   };
+//   let status = 200;
+
+//   // Apply query params
+//   if (!params.valid || params.valid !== 'true') {
+//     responseContent = {
+//       id: 'badRequest',
+//       message: 'Missing valid query parameter set to true',
+//     };
+//     status = 400;
+//   }
+
+//   // Return JSON
+//   return respondJSON(request, response, status, responseContent);
+// };
+
+// const getNotFound = (request, response, params, acceptedTypes) => {
+//   // Set the response data
+//   const responseContent = {
+//     id: 'notFound',
+//     message: 'The page you are looking for was not found.',
+//   };
+//   const status = 404;
+
+//   // Return JSON
+//   return respondJSON(request, response, status, responseContent);
+// };
+
+// module.exports = {
+//   getSuccess,
+//   createUser,
+//   getBadRequest,
+//   getNotFound,
+// };
--- src/server.js
+++ src/server.js
@@ -7,24 +7,87 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': htmlHandler.getIndex,
-  '/style.css': htmlHandler.getStyle,
-  '/success': jsonHandler.getSuccess, // 200
-  '/badRequest': jsonHandler.getBadRequest, // 400 OR 200
-  notFound: jsonHandler.getNotFound,
-  index: htmlHandler.getIndex,
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getStyle,
+    '/getUsers': jsonHandler.getUsers,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+};
+
+const handleGet = (request, response, parsedUrl) => {
+    // Verify that the function exists in urlStruct
+    if(urlStruct['GET'][parsedUrl.pathname]) {
+        urlStruct['GET'][parsedUrl.pathname](request, response);
+    } else {
+        urlStruct['GET']['notFound'](request, response);
+    }
+}
+
+const handleHead = (request, response, parsedUrl) => {
+    // Verify that the function exists in urlStruct
+    if(urlStruct['HEAD'][parsedUrl.pathname]) {
+        urlStruct['HEAD'][parsedUrl.pathname](request, response);
+    } else {
+        urlStruct['HEAD']['notFound'](request, response);
+    }
+}
+
+const handlePost = (request, response, parsedUrl) => {
+    // Verify that the function exists in urlStruct
+    if(parsedUrl.pathname === '/addUser') {
+        const body = [];
+
+        // If an error happens, print to the console and make the response a 400-error
+        request.on('error', (err) => {
+            console.dir(err);
+            response.statusCode = 400;
+            response.end();
+        });
+
+        // Load each packet of data into one body array
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        // After the body is finished loading, turn the body into one concatenated string
+        request.on('end', () => {
+            const bodyString = Buffer.concat(body).toString();
+            const bodyParams = query.parse(bodyString);
+
+            console.log("request on end");
+            console.dir(bodyParams);
+
+            jsonHandler.addUser(request, response, bodyParams);
+        });
+    } else {
+        jsonHandler.notFound(request, response);
+    }
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-  const params = query.parse(parsedUrl.query);
-  const acceptedTypes = request.headers.accept.split(',');
 
-  console.log(`Requested URL: ${parsedUrl.pathname}`);
-  if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, params, acceptedTypes);
+  console.dir(parsedUrl.pathname);
+  console.dir(request.method);
+
+  if(request.method === 'GET') {
+    handleGet(request, response, parsedUrl);
+  } else if(request.method === 'HEAD') {
+    handleHead(request, response, parsedUrl);
+  } else if(request.method === 'POST') {
+    // console.dir(request);
+    // console.log("----------");
+    // console.dir(response);
+    // console.log("----------");
+    // console.dir(parsedUrl);
+    handlePost(request, response, parsedUrl);
   } else {
-    urlStruct.index(request, response);
+    jsonHandler.notFound(request, response);
   }
 };
 
