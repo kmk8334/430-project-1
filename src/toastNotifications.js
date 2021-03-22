f1d73b1ea682dfeaf9d90c675cf2f0a296ee6942
Kevin Kulp <kmk8334@rit.edu>
2021-03-21 21:33:10 -0400

    Format notification messages

--- src/toastNotifications.js
+++ src/toastNotifications.js
@@ -1,9 +1,7 @@
+// Set the header and body of the toast notification to the content,
+// then show it in the top-right corner
 let generateToast = (toastType, header, body) => {
     $('#toast-header-content').text(header);
     $('#toast-body-content').text(body);
     $('.toast').toast('show');
-}
-
-module.exports = {
-    generateToast,
 }
